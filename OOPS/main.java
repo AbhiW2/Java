24a76ba5a0ccc38df1027fdd42a1bb6b9e60e8f0
Abhi Wargad <wargadabhi@gmail.com>
2026-05-14 17:12:41 +0530

    added basic oops

---
+++ OOPS/main.java
@@ -0,0 +1,24 @@
+package OOPS;
+
+
+class Student{
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class main {
+    public static void main(String[]args){
+        Student s = new Student();
+
+        s.setName("Abhi");
+
+        System.out.println(s.getName());
+    }
+}
