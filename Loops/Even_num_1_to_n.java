315fb250066987708d6b9784713cf67b7e26db65
Abhi Wargad <wargadabhi@gmail.com>
2026-05-22 10:30:55 +0530

    even num

---
+++ Loops/Even_num_1_to_n.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class Even_num_1_to_n {
+    public static void main(String[] args) {
+        int n = 100;
+        for (int i=0;i<n;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }    
+}
