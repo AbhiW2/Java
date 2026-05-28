3061ec54cf11322315099b89b39adc9d4460083c
Abhi wargad <Wargadabhi@gmail.com>
2026-05-28 12:08:35 +0530

    Add FactorialNumber class to calculate factorial

---
+++ Loops/Factorial_num.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FactorialNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+        long factorial = 1;
+
+        while (i <= n) {
+            factorial = factorial * i;
+            i++;
+        }
+
+        System.out.println("Factorial = " + factorial);
+    }
+}
