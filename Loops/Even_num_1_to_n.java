67deb2864740ce3497aeeb0a60459c0700b7bea0
Abhi Wargad <wargadabhi@gmail.com>
2026-06-04 20:03:07 +0530

    take n from user change

--- Loops/Even_num_1_to_n.java
+++ Loops/Even_num_1_to_n.java
@@ -1,10 +1,14 @@
 package Loops;
 
+import java.util.Scanner;
 public class Even_num_1_to_n {
     public static void main(String[] args) {
-        int n = 100;
-        for (int i=0;i<n;i++){
-            if(i%2==0){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int n = scanner.nextInt();
+        System.out.println("Even numbers from 1 to " + n + ":");
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
         }
