6cc204e15735e5faf0831e7713faf444d12d55ef
AbhiW2 <wargadabhi@gmail.com>
2026-09-04 22:25:10 +0530

    added missing num code

---
+++ Arrays/Find_Missing_Num.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class Find_Missing_Num {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 4, 5, 6};
+
+        int n = 6;
+
+        int expectedSum = n * (n + 1) / 2;
+
+        int actualSum = 0;
+
+        for (int num : arr) {
+            actualSum += num;
+        }
+
+        int missing = expectedSum - actualSum;
+
+        System.out.println("Missing number: " + missing);
+    }
+}
