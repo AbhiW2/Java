6bf4611ef8d0197e5daa6f889f7e3ecd5b22fd66
AbhiW2 <wargadabhi@gmail.com>
2026-09-06 23:03:14 +0530

    added move zeros end code

---
+++ Arrays/Move_zeros_end.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class Move_zeros_end {
+    public static void main(String[] args) {
+
+        int[] arr = {0, 1, 0, 3, 12};
+
+        int index = 0;
+
+        // Put all non-zero elements at the beginning
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        // Fill remaining positions with zeros
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+
+        // Print array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
